cf94bb8ea17236d59e59dce6ad8e011bc23d564c
nature <nature.kim@gmail.com>
2022-11-17 19:58:03 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/post_list.html'
 
-def index(request) :
-    posts = Post.objects.all().order_by('-pk')
+# FBV 방식 주석처리
+# def index(request) :
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render (
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
+
+def single_post_page(request, pk) :
+    post = Post.objects.get(pk=pk)
 
     return render (
         request,
-        'blog/index.html',
+        'blog/single_post_page.html',
         {
-            'posts' : posts,
+            'post' : post,
         }
     )
